00d999e62ed959ff8e2aa7c6a9e66daec6f7587b
B-Whale-block <bwhale.bbw@gmail.com>
2024-12-21 16:54:34 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Mobile Nav
     const menuToggle = document.getElementById('menuToggle');
     const navMenu = document.querySelector('.navbar');
     
@@ -12,6 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
     if (menuToggle && navMenu) {
         menuToggle.addEventListener('click', () => {
             navMenu.classList.toggle('active');
+        });
+
+        // Close nav menu if clicked outside
+        document.addEventListener('click', (e) => {
+            if (!navMenu.contains(e.target) && !menuToggle.contains(e.target)) {
+                navMenu.classList.remove('active');
+            }
         });
     }
 
